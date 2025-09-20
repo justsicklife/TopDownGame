cfac4edd3cea60464979c62af4008881f890e680
justsicklife <justsicklife@gmail.com>
2025-09-20 15:40:50 +0900

    게임 저장 기능 추가

---
+++ Assets/SaveController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Cinemachine;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+
+    private string saveLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+
+        LoadGame();
+    }
+
+    public void saveGame()
+    {
+        SaveData saveDeta = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D.gameObject.name
+        };
+
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveDeta));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+
+            FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D = GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        }
+        else
+        {
+            saveGame();
+        }
+    }
+
+}
