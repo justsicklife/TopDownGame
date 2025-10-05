bc0a377f2262e411b85197a716dc638bcb51f363
justsicklife <justsicklife@gmail.com>
2025-10-06 00:37:01 +0900

    젤리피쉬 애니메이션 추가

--- Assets/JellyFish.cs
+++ Assets/JellyFish.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public enum JellyFishState
@@ -21,8 +22,13 @@ public class JellyFish : MonoBehaviour
 
     public float colliderRadius = 5f;
 
+    public Vector2 direction;
+
+    private Animator animator;
+
     void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         detectionRange = gameObject.GetComponentInChildren<DetectionRange>();
         detectionRange.GetComponent<CircleCollider2D>().radius = colliderRadius;
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -43,12 +49,31 @@ public class JellyFish : MonoBehaviour
         {
             case JellyFishState.Idle:
                 rigidbody2D.velocity = Vector2.zero;
+                animator.SetFloat("LastInputX", direction.x);
+                animator.SetFloat("LastInputY", direction.y);
+                animator.SetBool("IsWalking", false);
                 break;
             case JellyFishState.Chase:
-                Vector2 direction = (detectionRange.playerPos -(Vector2)this.transform.position).normalized;
-                rigidbody2D.velocity = direction * speed;
-                break; 
+                Vector2 directionPlayer = (detectionRange.playerPos - (Vector2)this.transform.position).normalized;
+                rigidbody2D.velocity = directionPlayer * speed;
+                SetAnimator(directionPlayer);
+                animator.SetFloat("InputX", direction.x);
+                animator.SetFloat("InputY", direction.y);
+                animator.SetBool("IsWalking", true);
+                break;
         }
+    }
+
+    private void SetAnimator(Vector2 dir)
+    {
+        float dirX = dir.x;
+        float dirY = dir.y;
+
+        // 0.5 이상이면 1, -0.5 이하이면 -1, 그 외는 0
+        dirX = Mathf.Abs(dirX) > 0.5f ? Mathf.Sign(dirX) : 0f;
+        dirY = Mathf.Abs(dirY) > 0.5f ? Mathf.Sign(dirY) : 0f;
+
+        direction = new Vector2(dirX, dirY);
     }
 
 }
