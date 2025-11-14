daf7f0f782accf1c53da263be44ed7013fb689db
justsicklife <justsicklife@gmail.com>
2025-11-14 15:36:22 +0900

    일단 커밋

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
         if (GameManager.Instance.IsDialoguePlaying)
         {
             animator.SetBool("isWalking", false);
+            moveInput = Vector2.zero;
             return;
         }
 
